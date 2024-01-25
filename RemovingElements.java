9692aa6f3fa837b0fb78f37b6849dec7373e9912
ray <2313715475@qq.com>
2024-01-25 11:36:01 +0800

    双向指针

--- RemovingElements.java
+++ RemovingElements.java
@@ -9,6 +9,9 @@
  */
 public class RemovingElements {
 
+
+
+
     //暴力解法
     class Solution {
         public int removeElement(int[] nums, int val) {
@@ -25,6 +28,64 @@ public class RemovingElements {
             return len;
         }
     }
-    
+
+    // 快慢指针法，时间复杂度：O(n)
+    // 空间复杂度：O(1)
+    class Solution1 {
+        public int removeElement(int[] nums, int val) {
+            int fast = 0;
+            int slow = 0;
+            int len = nums.length;
+            for( fast = 0;fast<len;fast++ ){
+                if(nums[fast]!= val){
+                    nums[slow] = nums[fast];
+                    slow++;
+                }
+
+
+            }
+            return slow;
+        }
+    }
+
+    //相向双指针法
+    class Solution2 {
+        public int removeElement(int[] nums, int val) {
+            int left = 0;
+            int right = nums.length - 1;
+            while(right >= 0 && nums[right] == val) right--; //将right移到从右数第一个值不为val的位置
+            while(left <= right) {
+                if(nums[left] == val) { //left位置的元素需要移除
+                    //将right位置的元素移到left（覆盖），right位置移除
+                    nums[left] = nums[right];
+                    right--;
+                }
+                left++;
+                while(right >= 0 && nums[right] == val) right--;
+            }
+            return left;
+        }
+    }
+
+    // 相向双指针法（版本二）
+    class Solution3 {
+        public int removeElement(int[] nums, int val) {
+            int left = 0;
+            int right = nums.length -1;
+            int len = nums.length;
+
+            while(left <= right){
+                if(nums[left] == val){
+                    nums[left] = nums[right];
+                    right--;
+                }else{
+                    left++;  // 这里兼容了right指针指向的值与val相等的情况
+                }
+            }
+
+            return left;
+        }
+    }
+
 
 }
